8c48cfcccdab1f97010baf49d0f942eb32aff0b2
oonik <aniksarkar0177@gmail.com>
2023-01-15 17:04:31 +0600

    third question add

--- src/component/Question/Question.js
+++ src/component/Question/Question.js
@@ -7,6 +7,8 @@ const Question = () => {
             <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
             <h4>2. props and state difference?</h4>
             <p>props are passed via component properties, they're not reactive. State are variables that react will react , updating the UI when values changes.</p>
+            <h4>3. What does useEffect do?</h4>
+            <p>By using this Hook, you tell React that your component needs to do something after render. React will remember the function you passed (we’ll refer to it as our “effect”), and call it later after performing the DOM updates. In this effect, we set the document title, but we could also perform data fetching or call some other imperative API.</p>
         </div>
     );
 };
