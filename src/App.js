706f0c4d712e47004fb1061c737e5f5b5584550d
oonik <aniksarkar0177@gmail.com>
2023-01-15 16:54:00 +0600

    react props and state difference

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header/Header';
 import Fitness from './component/Fitness/Fitness';
+import Question from './component/Question/Question';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Fitness></Fitness>
-      
+      <Question></Question>
     </div>
   );
 }
--- src/component/Question/Question.js
+++ src/component/Question/Question.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import './Question.css'
 const Question = () => {
     return (
-        <div>
+        <div className='question-cotainer'>
+            <h4>1. How dose react work?</h4>
             <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
+            <h4>2. props and state difference?</h4>
+            <p>props are passed via component properties, they're not reactive. State are variables that react will react , updating the UI when values changes.</p>
         </div>
     );
 };
